25ee747de5492285937692226eb8f86521b70857
JR Tashjian <jrtashjian@gmail.com>
2022-04-22 23:00:54 -0400

    Add microformats to post listing and make compact

--- source/_partials/all-posts.blade.php
+++ source/_partials/all-posts.blade.php
@@ -3,9 +3,13 @@
 		<h2>Archive</h2>
 		<ul>
 			@foreach( $posts as $entry )
-				<li class="my-3">
-					<a href="{{ $entry->getUrl() }}">{{ $entry->title }}</a>
-					<span class="block">{{ $entry->getDate()->format( 'Y-m-d' ) }}</span>
+				<li class="my-1.5">
+					<article class="h-entry sm:flex flex-row-reverse justify-end">
+						<h2 class="my-0 font-normal">
+							<a class="u-url" rel="bookmark" href="{{ $entry->getUrl() }}">{{ $entry->title }}</a>
+						</h2>
+						<time class="dt-published mr-3" datetime="{{ date( DATE_ATOM, $entry->date ) }}" title="{{ $entry->getDate()->format( 'Y-m-d' ) }}">{{ $entry->getDate()->format( 'Y-m-d' ) }}</time>
+					</article>
 				</li>
 			@endforeach
 		</ul>
