0c01f47843383e956a346828d309bc263b1c5992
JR Tashjian <jrtashjian@gmail.com>
2022-04-23 12:01:45 -0400

    Add microformats to post page

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -1,10 +1,13 @@
 @extends('_layouts.master')
 
 @section('body')
-	<article>
-		<h1>{{ $page->title }}</h1>
-		<p class="mt-0">{{ $page->getDate()->format( 'F j, Y' ) }}</p>
+	<article class="h-entry">
+		<h1 class="p-name">{{ $page->title }}</h1>
+		<time class="dt-published mr-3" datetime="{{ date( DATE_ATOM, $page->date ) }}" title="Published on {{ $page->getDate()->format( 'F j, Y' ) }}">{{ $page->getDate()->format( 'F j, Y' ) }}</time>
+		<a class="p-author h-card sr-only" href="http://jrtashjian.com">JR Tashjian</a>
 
-		@yield('content')
+		<div class="e-content">
+			@yield('content')
+		</div>
 	</article>
 @endsection
--- source/_partials/all-posts.blade.php
+++ source/_partials/all-posts.blade.php
@@ -8,7 +8,7 @@
 						<h2 class="my-0 font-normal">
 							<a class="u-url" rel="bookmark" href="{{ $entry->getUrl() }}">{{ $entry->title }}</a>
 						</h2>
-						<time class="dt-published mr-3" datetime="{{ date( DATE_ATOM, $entry->date ) }}" title="{{ $entry->getDate()->format( 'Y-m-d' ) }}">{{ $entry->getDate()->format( 'Y-m-d' ) }}</time>
+						<time class="dt-published mr-3" datetime="{{ date( DATE_ATOM, $entry->date ) }}" title="Published on {{ $entry->getDate()->format( 'F j, Y' ) }}">{{ $entry->getDate()->format( 'Y-m-d' ) }}</time>
 					</article>
 				</li>
 			@endforeach
