aefdda14103188cc284b8642c2430cbb323dedd6
JR Tashjian <jrtashjian@gmail.com>
2022-04-28 00:59:47 -0400

    Make pixelated image a background image

--- source/_layouts/frontpage.blade.php
+++ source/_layouts/frontpage.blade.php
@@ -1,9 +1,8 @@
 @extends('_layouts.master')
 
 @section('body')
-	<figure>
+	<figure style="background-image:url(/assets/images/about-page-pixel.webp);">
 		<img src="/assets/images/about-page.webp" height="400" width="720" alt="Portrait of JR Tashjian" class="image-full" loading="lazy" />
-		<img src="/assets/images/about-page-pixel.webp" height="400" width="720" alt="Pixelated portrait of JR Tashjian" />
 	</figure>
 
 	@yield('content')
