78c3ff98495093ce9854e9f918ef4977924d5dc2
JR Tashjian <jrtashjian@gmail.com>
2022-04-29 16:54:12 -0400

    Fix post header spacing

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -2,9 +2,11 @@
 
 @section('body')
 	<article class="h-entry">
-		<h1 class="p-name">{{ $page->title }}</h1>
-		<time class="dt-published mr-3" datetime="{{ date( DATE_ATOM, $page->date ) }}" title="Published on {{ $page->getDate()->format( 'F j, Y' ) }}">{{ $page->getDate()->format( 'F j, Y' ) }}</time>
-		<a class="p-author h-card sr-only" href="http://jrtashjian.com">JR Tashjian</a>
+		<header>
+			<h1 class="p-name">{{ $page->title }}</h1>
+			<time class="dt-published mr-3" datetime="{{ date( DATE_ATOM, $page->date ) }}" title="Published on {{ $page->getDate()->format( 'F j, Y' ) }}">{{ $page->getDate()->format( 'F j, Y' ) }}</time>
+			<a class="p-author h-card sr-only" href="http://jrtashjian.com">JR Tashjian</a>
+		</header>
 
 		@if( $page->alert_message )
 			<div class="alert alert--{{ $page->alert_type }}" role="alert">
