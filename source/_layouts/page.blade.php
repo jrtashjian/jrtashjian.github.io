73098cf1bd3244f800fe7e0af8b5d6f41470c994
JR Tashjian <jrtashjian@gmail.com>
2022-05-02 22:49:01 -0400

    Tweak page header

--- source/_layouts/page.blade.php
+++ source/_layouts/page.blade.php
@@ -2,10 +2,12 @@
 
 @section('body')
 	<section class="mt-0">
-		<h1 class="p-name">{{ $page->title }}</h1>
-		@if( $page->date )
-			<time class="dt-updated mr-3" datetime="{{ date( DATE_ATOM, $page->date ) }}" title="Last updated on {{ $page->getDate()->format( 'F j, Y' ) }}">Last updated on {{ $page->getDate()->format( 'F j, Y' ) }}</time>
-		@endif
+		<header>
+			<h1 class="p-name">{{ $page->title }}</h1>
+			@if( $page->date )
+				<time class="dt-updated mr-3" datetime="{{ date( DATE_ATOM, $page->date ) }}" title="Last updated on {{ $page->getDate()->format( 'F j, Y' ) }}">Last updated on {{ $page->getDate()->format( 'F j, Y' ) }}</time>
+			@endif
+		</header>
 
 		<div class="e-content">
 			@yield('content')
