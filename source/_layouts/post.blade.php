27dbabea2dabadd517e590e12ccec363e690e092
JR Tashjian <hello@jrtashjian.com>
2023-04-20 21:36:39 -0400

    remove link on author line

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -5,7 +5,7 @@
 		<header>
 			<h1 class="p-name">{{ $page->title }}</h1>
 			<time class="dt-published mr-3" datetime="{{ date( DATE_ATOM, $page->date ) }}" title="Published on {{ $page->getDate()->format( 'F j, Y' ) }}">{{ $page->getDate()->format( 'F j, Y' ) }}</time>
-			<a class="p-author h-card sr-only" href="http://jrtashjian.com">JR Tashjian</a>
+			<span class="p-author h-card sr-only" href="http://jrtashjian.com">JR Tashjian</span>
 		</header>
 
 		@if( $page->alert_message )
