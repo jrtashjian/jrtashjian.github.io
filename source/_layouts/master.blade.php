d33d91280bd0154bd53f4d3a32b485311cf6391e
JR Tashjian <hello@jrtashjian.com>
2023-04-20 21:37:05 -0400

    Bump base font size

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -38,7 +38,7 @@
 
         <link rel="alternate" type="application/rss+xml" title="All Posts by {{ $page->siteName }} (The RSS Feed)" href="{{ $page->baseUrl }}/feed.xml" />
     </head>
-    <body class="bg-white dark:bg-black text-black dark:text-white font-mono leading-relaxed px-4">
+    <body class="bg-white dark:bg-black text-black dark:text-white font-mono text-lg leading-relaxed px-4">
         <div class="mx-auto max-w-prose">
 
             <header class="flex flex-wrap mb-8 py-8 border-b-8 border-black dark:border-white" role="banner">
